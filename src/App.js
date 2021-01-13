9b30bccf2b4aeee4b6875d01ddea340687741287
Siraj Khan <sirajrkhan@live.in>
2021-01-13 10:51:32 +0530

    updated docker config

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Bismillah</h1>
+      <h1>Bismillah... Alhamdulillah</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
