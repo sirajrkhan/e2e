1b87db4b28958977eaf0997c67819824a2ff4327
Siraj Khan <sirajrkhan@live.in>
2021-01-13 10:08:05 +0530

    dockering...

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Bismillah</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save IT to reload.
         </p>
         <a
           className="App-link"
