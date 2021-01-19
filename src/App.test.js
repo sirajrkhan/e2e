eb446a31133851ec1a464039333329eedbd73373
Siraj Khan <sirajrkhan@live.in>
2021-01-19 16:19:32 +0530

    updated the test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders working', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Working/i);
   expect(linkElement).toBeInTheDocument();
 });
 
